d59b5b7c1e392f3a2a9f26fef2a5784ae9443f3a
gergo225 <fazekasgergo225@gmail.com>
2020-10-07 22:13:56 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# The new Practice Git & GitHub
+
+## Should be a separate branch
+The goal is to somehow create a fully separate master branch, independent of the 'old-version'
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello, NEW World!";
+	
+	// btw. this is just a placeholder file, it doesn't have any real usage
+	cout << "Now I must end this program, bye!";
+
+	return 0;
+}
